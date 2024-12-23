63c1701aa4e28cba371865e761e988ffe5b7e706
Dana Szova <danaszova@Danas-MBP.lan>
2024-12-23 10:38:53 -0700

    bid stream working with DSP competetive

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { startBidStream } from '../bidStream/bid-stream';
+import { startMockDSP } from '../DSP/mock-dsp';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log('NestJS application is running on http://localhost:3000');
+
+  // Start the Mock DSP Server
+  startMockDSP();
+
+  // Start the Bid Stream Simulation
+  startBidStream();
 }
 bootstrap();
